9f4d06fc16f5922c3dc684dd030d555dfa7eb66b
fauzi niko <fauziniko12@gmail.com>
2025-02-18 00:22:59 +0700

    [Misc] Update: src/routes/list.js,

--- src/routes/list.js
+++ src/routes/list.js
@@ -2,6 +2,26 @@ const express = require('express');
 const router = express.Router();
 const { listMidiFiles } = require('../controllers/listController');
 
+/**
+ * @swagger
+ * /api/files:
+ *   get:
+ *     summary: Get list of MIDI files
+ *     responses:
+ *       200:
+ *         description: List of MIDI files
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 files:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *       500:
+ *         description: Error reading MIDI files from MinIO
+ */
 router.get('/files', listMidiFiles);
 
 module.exports = router;
